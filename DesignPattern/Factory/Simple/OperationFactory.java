b016125dad5ab7a3df64d888bddff8ab84620f30
why <1459714629@qq.com>
2018-03-12 21:15:22 +0800

    Singleton&SimpleFactory

---
+++ DesignPattern/Factory/Simple/OperationFactory.java
@@ -0,0 +1,32 @@
+package DesignPattern.Factory.Simple;
+
+/**
+ * 操作类工厂类
+ * 
+ * @author Hongyue Wang
+ *
+ */
+public class OperationFactory {
+    public static Operation createOperation(char operator) {
+	Operation operation = null;
+
+	switch (operator) {
+	case '+':
+	    operation = new OperationAdd();
+	    break;
+	case '-':
+	    operation = new OperationSub();
+	    break;
+	case '*':
+	    operation = new OperationMul();
+	    break;
+	case '/':
+	    operation = new OperationDiv();
+	    break;
+	default:
+	    throw new RuntimeException("unsupported operation");
+	}
+
+	return operation;
+    }
+}
